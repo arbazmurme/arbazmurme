f87fb658e0691d283ac8741ee88888d3fb240ca1
arbaj murme <arbaj.murme@lejhro.com>
2024-09-02 21:39:03 +0530

    animation added 1

--- src/app/template.jsx
+++ src/app/template.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { animatePageIn } from "@/context/animations"
+import animatePageIn from "@/context/animations"
 import { useEffect } from "react"
 
 export default function Template({ children }) {
--- src/context/animations.jsx
+++ src/context/animations.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import gsap from "gsap";
 import { useRouter } from "next/router";
 
-export const animatePageIn = () => {
+const animatePageIn = () => {
   const bannerOne = document.getElementById("banner-1");
   const bannerTwo = document.getElementById("banner-2");
   const bannerThree = document.getElementById("banner-3");
@@ -19,26 +19,27 @@ export const animatePageIn = () => {
     });
   }
 };
+export default animatePageIn;
 
-export const animatePageOut = (href) => {
-  const router = useRouter();
+// export const animatePageOut = (href) => {
+//   const router = useRouter();
 
-  const bannerOne = document.getElementById("banner-1");
-  const bannerTwo = document.getElementById("banner-2");
-  const bannerThree = document.getElementById("banner-3");
-  const bannerFour = document.getElementById("banner-4");
+//   const bannerOne = document.getElementById("banner-1");
+//   const bannerTwo = document.getElementById("banner-2");
+//   const bannerThree = document.getElementById("banner-3");
+//   const bannerFour = document.getElementById("banner-4");
 
-  if (bannerOne && bannerTwo && bannerThree && bannerFour) {
-    const tl = gsap.timeline();
+//   if (bannerOne && bannerTwo && bannerThree && bannerFour) {
+//     const tl = gsap.timeline();
 
-    tl.set([bannerOne, bannerTwo, bannerThree, bannerFour], {
-      yPercent: -100,
-    }).to([bannerOne, bannerTwo, bannerThree, bannerFour], {
-      yPercent: 0,
-      stagger: 0.2,
-      onComplete: () => {
-        router.push(href);
-      },
-    });
-  }
-};
+//     tl.set([bannerOne, bannerTwo, bannerThree, bannerFour], {
+//       yPercent: -100,
+//     }).to([bannerOne, bannerTwo, bannerThree, bannerFour], {
+//       yPercent: 0,
+//       stagger: 0.2,
+//       onComplete: () => {
+//         router.push(href);
+//       },
+//     });
+//   }
+// };
