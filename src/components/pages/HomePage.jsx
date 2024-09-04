b311b6b0bd8857e9a69f15279664ecca67499afc
arbaj murme <arbaj.murme@lejhro.com>
2024-09-04 10:11:27 +0530

    home page added

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,10 +1,71 @@
-const HomePage = () => {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <h1 className="text-2xl font-bold">HomePage</h1>
+import Image from "next/image";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+const HomeDetails = () => {
+  return (
+    <div className="flex flex-col lg:flex-row h-screen items-center">
+      {/* Background Image Section */}
+      <div
+        className="hidden lg:block lg:w-1/2 bg-cover bg-center fixed h-screen"
+        style={{ backgroundImage: "url(/home-bg.png)" }}
+      >
+        <div className="min-h-screen flex justify-center items-center">
+          <Image
+            alt="hero man"
+            src="/aa.jpg"
+            width={450}
+            height={450}
+            className="img-fluid mx-auto rounded-3xl w-[500px] h-[620px] ml-[40px]"
+            loading="lazy"
+          />
+        </div>
       </div>
-    );
-  };
-  
-  export default HomePage;
-  
+
+      {/* Text and Image Section */}
+      <div className="w-full lg:w-1/2 lg:ml-auto text-center lg:text-left p-8 lg:p-16">
+        {/* Mobile Image */}
+        <div className="lg:hidden mb-8">
+          <Image
+            alt="hero man"
+            src="/aa.jpg"
+            width={300}
+            height={300}
+            className="img-fluid mx-auto"
+            loading="lazy"
+          />
+        </div>
+
+        <h1 className="text-4xl lg:text-5xl font-bold">
+          I Am Arbaz Murme.
+        </h1>
+        <span className="block text-4xl lg:text-5xl font-bold uppercase text-[#ffb400]">
+          Web Designer
+        </span>
+        <p className="mt-4 text-lg text-gray-400">
+          Web development intern specializing in front-end and back-end
+          technologies, passionate about creating responsive, user-friendly
+          websites. Dedicated to empowering the web through modern JavaScript
+          frameworks while gaining hands-on experience and contributing to
+          innovative projects.
+        </p>
+        <Link
+          href="#_"
+          class="mt-8 relative group overflow-hidden font-medium inline-block rounded-full border border-[#ffb400] "
+        >
+          <span class="absolute inset-0 bg-[#ffb400] transform translate-x-full transition-transform duration-300 ease-out group-hover:translate-x-0 opacity-90"></span>
+          <span class="relative flex items-center space-x-2">
+            <span className="mx-8 uppercase">More about me</span>
+            <ArrowRightIcon className="h-14 w-14 bg-[#ffb400] rounded-full p-4" />
+          </span>
+        </Link>
+      </div>
+      <div className="w-1/12 ">
+        <br />
+        <br />
+        <br />
+      </div>
+    </div>
+  );
+};
+
+export default HomeDetails;
