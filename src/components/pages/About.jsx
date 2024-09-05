0e3c93e10d96f0cc67512de2ea6b3b4cb6da34eb
arbaj murme <arbaj.murme@lejhro.com>
2024-09-05 23:47:23 +0530

    icone added

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,21 +1,30 @@
-import { ArrowRightIcon, LinkIcon } from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import {
+  IoLocation,
+  IoLanguageSharp,
+  IoMailUnread,
+  IoLogoLinkedin,
+} from "react-icons/io5";
+import { FaCode, FaSquareGithub, FaMobileScreenButton } from "react-icons/fa6";
+import { GiIndianPalace } from "react-icons/gi";
+import { SiFreelancer } from "react-icons/si";
 import Image from "next/image";
 import Link from "next/link";
 import "../../common/progress-bar.css";
 const About = () => {
   return (
-    <div class="max-w-7xl mx-auto px-4">
-      <div class="mx-auto text-center mt-12">
-        <h1 class="text-4xl font-bold text-gray-900 leading-tight mb-2 pb-4 relative">
-          <span class="bg-clip-text text-transparent bg-gradient-to-r from-[#ffb400] to-pink-500">
-            ABOUT <span class="text-primary">ME</span>
+    <div className="max-w-7xl mx-auto px-4">
+      <div className="mx-auto text-center mt-12">
+        <h1 className="text-4xl font-bold text-gray-900 leading-tight mb-2 pb-4 relative">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#ffb400] to-pink-500">
+            ABOUT <span className="text-primary">ME</span>
           </span>
-          <span class="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-r from-[#ffb400] to-pink-500"></span>
+          <span className="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-r from-[#ffb400] to-pink-500"></span>
         </h1>
       </div>
-      <div class="sm:flex items-center max-w-screen-xl">
-        <div class="sm:w-1/2 p-10">
-          <div class="image object-center text-center">
+      <div className="sm:flex items-center">
+        <div className="sm:w-1/2 p-10">
+          <div className="image object-center text-center">
             <Image
               height={500}
               width={500}
@@ -24,61 +33,71 @@ const About = () => {
             />
           </div>
         </div>
-        <div class="sm:w-1/2 p-5">
-          <h2 class="my-4 font-bold text-3xl sm:text-4xl uppercase">
-            personal <span class="text-[#ffb400]">infos</span>
+        <div className="sm:w-1/2 p-5">
+          <h2 className="my-4 font-bold text-3xl sm:text-4xl uppercase">
+            personal <span className="text-[#ffb400]">infos</span>
           </h2>
-          <div class="flex flex-col md:flex-row justify-between">
-            <div class="md:w-1/2 p-4">
-              <ul class="list-none">
-                <li class="mb-4 flex items-center">
+          <div className="flex flex-col md:flex-row justify-between">
+            <div className="md:w-1/2 p-4">
+              <ul className="list-none">
+                <li className="mb-4 flex items-center">
+                  <FaCode className="text-[#ffb400] mr-2"/>
                   <strong>First Name&nbsp;:&nbsp;</strong> Arbaz
                 </li>
-                <li class="mb-4 flex items-center">
+                <li className="mb-4 flex items-center">
+                  <FaCode className="text-[#ffb400] mr-2"/>
                   <strong>Last Name&nbsp;:&nbsp;</strong> Murme
                 </li>
-                <li class="mb-4 flex items-center">
+                <li className="mb-4 flex items-center">
+                  <IoLocation className="text-[#ffb400] mr-2"/>
                   <strong>Address&nbsp;:&nbsp;</strong> Maharashtra - Solapur
                 </li>
-                <li class="mb-4 flex items-center">
+                <li className="mb-4 flex items-center">
+                  <GiIndianPalace className="text-[#ffb400] mr-2"/>
                   <strong>Nationality&nbsp;:&nbsp;</strong> India
                 </li>
-                <li class="mb-4 flex items-center">
-                  <strong>Languages&nbsp;:&nbsp;</strong>English, Hindi, Marathi
+                <li className="mb-4 flex items-center">
+                  <IoLanguageSharp className="text-[#ffb400] mr-2"/>
+                  <strong>Languages&nbsp;:&nbsp;</strong>English, Hindi
                 </li>
               </ul>
             </div>
-            <div class="md:w-1/2 p-4">
-              <ul class="list-none">
-                <li class="mb-4 flex items-center">
+            <div className="md:w-1/2 p-4">
+              <ul className="list-none">
+                <li className="mb-4 flex items-center">
+                  <IoMailUnread className="text-[#ffb400] mr-2"/>
                   <strong>Email&nbsp;:&nbsp;</strong>
-                  <Link href={"arbazmurme@gamil.com"} class="text-blue-600">
+                  <Link href={"arbazmurme@gamil.com"} className="text-blue-600">
                     arbazmurme@mail.com
                   </Link>
                 </li>
-                <li class="mb-4 flex items-center">
+                <li className="mb-4 flex items-center">
+                  <FaMobileScreenButton className="text-[#ffb400] mr-2"/>
                   <strong>Phone&nbsp;:&nbsp;</strong>
-                  <a class="text-blue-600">+91 90281 21976</a>
+                  <a className="text-blue-600">+91 90281 21976</a>
                 </li>
-                <li class="mb-4 flex items-center">
+                <li className="mb-4 flex items-center">
+                  <IoLogoLinkedin className="text-[#ffb400] mr-2"/>
                   <strong>Linkedin&nbsp;:&nbsp;</strong>
                   <Link
                     href={"https://www.linkedin.com/in/arbaj-murme-4493031a3/"}
-                    class="text-blue-600"
+                    className="text-blue-600"
                   >
                     arbaj-murme-4493031a3
                   </Link>
                 </li>
-                <li class="mb-4 flex items-center">
+                <li className="mb-4 flex items-center">
+                  <FaSquareGithub className="text-[#ffb400] mr-2"/>
                   <strong>GitHub&nbsp;:&nbsp;</strong>
                   <Link
                     href={"https://github.com/arbazmurme"}
-                    class="text-blue-600"
+                    className="text-blue-600"
                   >
                     arbazmurme
                   </Link>
                 </li>
-                <li class="mb-4 flex items-center">
+                <li className="mb-4 flex items-center">
+                  <SiFreelancer className="text-[#ffb400] mr-2"/>
                   <strong>Freelance&nbsp;:&nbsp;</strong>{" "}
                   <samp className="text-[#ffb400]">Available</samp>
                 </li>
@@ -87,10 +106,10 @@ const About = () => {
           </div>
           <Link
             href="/arbaz-murme.pdf"
-            class="mt-8 relative group overflow-hidden font-medium inline-block rounded-full border border-[#ffb400] px-auto"
+            className="mt-8 relative group overflow-hidden font-medium inline-block rounded-full border border-[#ffb400] px-auto"
           >
-            <span class="absolute inset-0 bg-[#ffb400] transform translate-x-full transition-transform duration-300 ease-out group-hover:translate-x-0 opacity-90"></span>
-            <span class="relative flex items-center space-x-2">
+            <span className="absolute inset-0 bg-[#ffb400] transform translate-x-full transition-transform duration-300 ease-out group-hover:translate-x-0 opacity-90"></span>
+            <span className="relative flex items-center space-x-2">
               <span className="mx-8 uppercase">Download CV</span>
               <ArrowRightIcon className="h-14 w-14 bg-[#ffb400] rounded-full p-4" />
             </span>
@@ -99,7 +118,7 @@ const About = () => {
       </div>
 
       <div className="flex flex-wrap">
-        <div className="w-full md:w-1/4 p-4">
+        <div className="w-1/2 md:w-1/4 p-4">
           <div className=" shadow-lg rounded-lg p-6 text-center">
             <div
               role="progressbar"
@@ -114,7 +133,7 @@ const About = () => {
             <h3 className="text-lg font-bold">HTML 5</h3>
           </div>
         </div>
-        <div className="w-full md:w-1/4 p-4">
+        <div className="w-1/2 md:w-1/4 p-4">
           <div className="shadow-lg rounded-lg p-6 text-center">
             <div
               role="progressbar"
@@ -129,7 +148,7 @@ const About = () => {
             <h3 className="text-lg font-bold">CSS 3</h3>
           </div>
         </div>
-        <div className="w-full md:w-1/4 p-4">
+        <div className="w-1/2 md:w-1/4 p-4">
           <div className="m-auto shadow-lg rounded-lg p-6 text-center">
             <div
               role="progressbar"
@@ -144,7 +163,7 @@ const About = () => {
             <h3 className="text-lg font-bold   ">Bootstrap 5</h3>
           </div>
         </div>
-        <div className="w-full md:w-1/4 p-4">
+        <div className="w-1/2 md:w-1/4 p-4">
           <div className=" shadow-lg rounded-lg p-6">
             <div
               role="progressbar"
@@ -159,7 +178,7 @@ const About = () => {
             <h3 className="text-lg font-bold text-center">Tailwind CSS</h3>
           </div>
         </div>
-        <div className="w-full md:w-1/4 p-4">
+        <div className="w-1/2 md:w-1/4 p-4">
           <div className=" shadow-lg rounded-lg p-6 text-center">
             <div
               role="progressbar"
@@ -174,7 +193,7 @@ const About = () => {
             <h3 className="text-lg font-bold">React JSX</h3>
           </div>
         </div>
-        <div className="w-full md:w-1/4 p-4">
+        <div className="w-1/2 md:w-1/4 p-4">
           <div className=" shadow-lg rounded-lg p-6 text-center">
             <div
               role="progressbar"
@@ -189,7 +208,7 @@ const About = () => {
             <h3 className="text-lg font-bold">Next JSX</h3>
           </div>
         </div>
-        <div className="w-full md:w-1/4 p-4">
+        <div className="w-1/2 md:w-1/4 p-4">
           <div className=" shadow-lg rounded-lg p-6 text-center">
             <div
               role="progressbar"
@@ -204,7 +223,7 @@ const About = () => {
             <h3 className="text-lg font-bold">Node JS</h3>
           </div>
         </div>
-        <div className="w-full md:w-1/4 p-4">
+        <div className="w-1/2 md:w-1/4 p-4">
           <div className=" shadow-lg rounded-lg p-6 text-center">
             <div
               role="progressbar"
