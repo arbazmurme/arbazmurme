f186622f1ce3c6ad71e82eb881533a7999839d87
arbaj murme <arbaj.murme@lejhro.com>
2024-09-07 19:45:23 +0530

    btn fixd

--- src/components/pages/Work.jsx
+++ src/components/pages/Work.jsx
@@ -1,10 +1,113 @@
 const Work = () => {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <h1 className="text-2xl font-bold">Work</h1>
+  return (
+    <div className="row">
+      <div className="col-12">
+        <h3 className="text-uppercase pb-5 mb-0 text-start text-sm-center custom-title font-semibold">
+          Experience <span>&amp;</span> Education
+        </h3>
       </div>
-    );
-  };
-  
-  export default Work;
-  
+      <div className="col-lg-6 my-4">
+        <div className="resume-box">
+          <ul>
+            <li>
+              <div className="icon">
+                <i className="fa fa-briefcase"></i>
+              </div>
+              <span className="time font-medium text-uppercase">
+                2018 - Present
+              </span>
+              <h5 className="font-bold text-uppercase">
+                Web Developer<span className="place font-medium">Envato</span>
+              </h5>
+              <p className="font-light">
+                Lorem ipsum dolor sit amet, consectetur tempor incididunt ut
+                labore adipisicing elit.
+              </p>
+            </li>
+            <li>
+              <div className="icon">
+                <i className="fa fa-briefcase"></i>
+              </div>
+              <span className="time font-medium text-uppercase">
+                2013 - 2018
+              </span>
+              <h5 className="font-bold text-uppercase">
+                UI/UX Designer
+                <span className="place font-medium">Themeforest</span>
+              </h5>
+              <p className="font-light">
+                Lorem incididunt dolor sit amet, consectetur eiusmod dunt
+                doldunt dol elit, tempor incididunt.
+              </p>
+            </li>
+            <li>
+              <div className="icon">
+                <i className="fa fa-briefcase"></i>
+              </div>
+              <span className="time font-medium text-uppercase">
+                2005 - 2013
+              </span>
+              <h5 className="font-bold text-uppercase">
+                Consultant<span className="place font-medium">Videohive</span>
+              </h5>
+              <p className="font-light">
+                Lorem ipsum dolor sit amet, tempor incididunt ut labore
+                consectetur elit, sed do eiusmod tempor.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="col-lg-6 my-4">
+        <div className="resume-box">
+          <ul>
+            <li>
+              <div className="icon">
+                <i className="fa fa-graduation-cap"></i>
+              </div>
+              <span className="time font-medium text-uppercase">2015</span>
+              <h5 className="font-bold text-uppercase">
+                ENGINEERING DEGREE
+                <span className="place font-medium">OXFORD UNIVERSITY</span>
+              </h5>
+              <p className="font-light">
+                Lorem ipsum dolor sit amet, consectetur tempor incididunt ut
+                labore adipisicing elit.
+              </p>
+            </li>
+            <li>
+              <div className="icon">
+                <i className="fa fa-graduation-cap"></i>
+              </div>
+              <span className="time font-medium text-uppercase">2012</span>
+              <h5 className="font-bold text-uppercase">
+                MASTER DEGREE
+                <span className="place font-medium">KIEV UNIVERSITY</span>
+              </h5>
+              <p className="font-light">
+                Lorem incididunt dolor sit amet, consectetur eiusmod dunt
+                doldunt dol elit, tempor incididunt.
+              </p>
+            </li>
+            <li>
+              <div className="icon">
+                <i className="fa fa-graduation-cap"></i>
+              </div>
+              <span className="time font-medium text-uppercase">2009</span>
+              <h5 className="font-bold text-uppercase">
+                BACHELOR DEGREE
+                <span className="place font-medium">TUNIS HIGH SCHOOL</span>
+              </h5>
+              <p className="font-light">
+                Lorem ipsum dolor sit amet, tempor incididunt ut labore
+                consectetur elit, sed do eiusmod tempor.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
