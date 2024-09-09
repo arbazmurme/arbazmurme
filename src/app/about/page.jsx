63dd7627a56b5ee81bb2ba08ef9ef27cd500c71e
arbaj murme <arbaj.murme@lejhro.com>
2024-09-09 14:00:57 +0530

    2 page metadata added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,14 @@
 import About from "@/components/pages/About";
+import Head from "next/head";
 
 // app/page.jsx
 export default function Home() {
   return (
-    <About />
+    <>
+      <Head>
+        <title>Arbaz Murme | Portfolio</title>
+      </Head>
+      <About />
+    </>
   );
 }
